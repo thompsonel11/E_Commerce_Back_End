596aeed5b1fbc1f2d96687330d099e41139cd6dd
Liz Thompson <thompsonel2011@gmail.com>
2021-06-29 18:20:23 -0400

    PUTs not working

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,13 @@
+// 
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
-
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
-
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -21,26 +19,29 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         isDecimal: true,
-      }
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
-        model: `category`,
-        key: `id`,
+        model: 'category',
+        key: 'id',
       },
     },
-  },   
+  },
   {
     sequelize,
     timestamps: false,
@@ -49,5 +50,4 @@ Product.init(
     modelName: 'product',
   }
 );
-
 module.exports = Product;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,7 +105,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
