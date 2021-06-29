9a44f45cdf482e47cb4123a5e0b6a178f609dae8
Liz Thompson <thompsonel2011@gmail.com>
2021-06-29 10:20:48 -0400

    models/index

--- models/index.js
+++ models/index.js
@@ -5,14 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-class Product extends Model {}
-
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
+
 
 module.exports = {
   Product,
